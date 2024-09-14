9563be92c35717467ef488a8baebc0e9d55d695f
Nitish Sharma <nitish.sharma1186@gmail.com>
2024-09-14 23:37:25 +0530

    vercel fix

--- app.js
+++ app.js
@@ -351,4 +351,9 @@ app.get('/order/:id', async (req, res) => {
 
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app
